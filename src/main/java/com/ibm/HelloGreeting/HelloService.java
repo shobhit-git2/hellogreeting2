f18629a6e6c9e3594cc017b53f2a3dd9faec8838
shobhit-git2 <shobhit51924@gmail.com>
2019-12-03 18:40:07 +0530

    Update HelloService.java

--- src/main/java/com/ibm/HelloGreeting/HelloService.java
+++ src/main/java/com/ibm/HelloGreeting/HelloService.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloService {
 
-    @RequestMapping(path="/greeting", method= RequestMethod.GET)
+    @RequestMapping(path="/greeting2", method= RequestMethod.GET)
     public String sayHi(@RequestParam String name){
-    String message = "Hello"+" "+name;
+    String message = "Bye"+" "+name;
     return message;
     }
 }
